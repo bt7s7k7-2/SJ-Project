85a470008b66d68565cc85bf66320cdfc2bc327d
Branislav Trstenský <bt7s7k7@hotmail.sk>
2026-03-24 21:14:25 +0100

    Starting

---
+++ docs/index.typ
@@ -0,0 +1,216 @@
+#import "@preview/rubber-article:0.5.0": *
+#import "@preview/datify:1.0.0"
+
+#let _title = "Návrh a overenie metód spracovania a štrukturovania vedeckých textov z dokumentov vo formáte PDF"
+
+// Options reference: https://github.com/npikall/rubber-article/blob/main/src/styles.typ
+#show: article.with(
+  lang: "sk",
+  par-spacing: 1.5em,
+  par-first-line-indent: 0em,
+  text-size: 11pt,
+  text-font: "Arial",
+)
+
+#show heading: it => block({
+  let num = if it.numbering == none { none } else { counter(heading).display(it.numbering) + h(0.75em) }
+  num + it.body
+})
+
+#align(center)[
+  #title[Jazyk 7.5 — `simpleURL`]
+  Branislav Trstenský
+]
+
+#set raw(syntaxes: "bnf.sublime-syntax")
+
+= Gramatika
+
+Jazyk v BNF ako definovaný v zadaní.
+
+```bnf
+url := httpaddress | ftpaddress | telnetaddress | mailtoaddress
+httpaddress := "http://" hostport ["/" path] ["?" search]
+ftpaddress := "ftp://" login "/" path
+telnetaddress := "telnet://" login
+mailtoaddress := "mailto:" xalphas "@" hostname
+login := [user [":" password] "@"] hostport
+hostport := hostname [":" port]
+hostname := xalphas {"." xalphas}
+port := digits
+path := segment {"/" segment}
+search := xalphas {"+" xalphas}
+user := xalphas
+password := xalphas
+segment := {xalpha}
+xalphas := xalpha {xalpha}
+xalpha := alpha | digit
+digits := digit {digit}
+alpha := "A" | .. | "Z" | "a" | .. | "z"
+digit := "0" | .. | "9"
+```
+
+= Transformácia na LL1
+
+== Prepis na pravidlá gramatiky
+
+```BNF
+URL → HTTP_ADDRESS
+URL → FTP_ADDRESS
+URL → TELNET_ADDRESS
+URL → MAILTO_ADDRESS
+HTTP_ADDRESS → "http://" HOST_PORT
+HTTP_ADDRESS → "http://" HOST_PORT "/" PATH
+HTTP_ADDRESS → "http://" HOST_PORT "?" SEARCH
+HTTP_ADDRESS → "http://" HOST_PORT "/" PATH "?" SEARCH
+FTP_ADDRESS → "ftp://" LOGIN "/" PATH
+TELNET_ADDRESS → "telnet://" LOGIN
+MAILTO_ADDRESS → "mailto:" XALPHAS "@" HOSTNAME
+LOGIN → HOST_PORT
+LOGIN → USER "@" HOST_PORT
+LOGIN → USER ":" PASSWORD "@" HOST_PORT
+HOST_PORT → HOSTNAME
+HOST_PORT → HOSTNAME ":" PORT
+HOSTNAME → XALPHAS
+HOSTNAME → XALPHAS "." HOSTNAME
+PORT → DIGITS
+PATH → SEGMENT
+PATH → SEGMENT "/" PATH
+SEARCH → XALPHAS
+SEARCH → XALPHAS "+" SEARCH
+USER → XALPHAS
+PASSWORD → XALPHAS
+SEGMENT → XALPHAS SEGMENT
+SEGMENT → ε
+XALPHAS → XALPHA
+XALPHAS → XALPHA XALPHAS
+XALPHA → ALPHA
+XALPHA → DIGIT
+DIGITS → DIGIT
+DIGITS → DIGIT DIGITS
+ALPHA → "A" | .. | "Z" | "a" | .. | "z"
+DIGIT → "0" | .. | "9"
+```
+
+== Verifikácia že gramaticka je LL1
+
+=== Nájdenie konfliktov
+
+Množiny $"FIRST"_1$:
+- $F_1("URL") = F_1("HTTP_ADDRESS") union F_1("FTP_ADDRESS") union F_1("TELNET_ADDRESS") union F_1("MAILTO_ADDRESS") = {"h", "f", "t", "m"}$
+- $F_1("HTTP_ADDRESS") = {"h"}$
+- $F_1("FTP_ADDRESS") = {"f"}$
+- $F_1("TELNET_ADDRESS") = {"t"}$
+- $F_1("MAILTO_ADDRESS") = {"m"}$
+- $F_1("LOGIN") = F_1("HOST_PORT") union F_1("USER") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("HOST_PORT") = F_1("HOSTNAME") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("HOSTNAME") = F_1("XALPHAS") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("PORT") = F_1("DIGITS") = {0, dots, 9}$
+- $F_1("PATH") = F_1("SEGMENT") union {"/"} = {0, dots, 9, a, dots, z, A, dots, Z, ε, "/"}$
+- $F_1("SEARCH") = F_1("XALPHAS") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("USER") = F_1("XALPHAS") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("PASSWORD") = F_1("XALPHAS") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("SEGMENT") = F_1("XALPHAS") union {ε} = {0, dots, 9, a, dots, z, A, dots, Z, ε}$
+- $F_1("XALPHAS") = F_1("XALPHA") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("XALPHA") = F_1("DIGIT") union F_1("ALPHA") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("DIGITS") = F_1("DIGIT") = {0, dots, 9}$
+- $F_1("ALPHA") = {a, dots, z, A, dots, Z}$
+- $F_1("DIGIT") = {0, dots, 9}$
+
+*Pre $"URL"$*:
+- $F_1("HTTP_ADDRESS") = {"h"}$
+- $F_1("FTP_ADDRESS") = {"f"}$
+- $F_1("TELNET_ADDRESS") = {"t"}$
+- $F_1("MAILTO_ADDRESS") = {"m"}$
+- #text(olive)[Žiadny konflikt]
+
+*Pre $"HTTP_ADDRESS"$*:
+- $F_1("http:// HOST_PORT") = {"h"}$
+- $F_1("http:// HOST_PORT / PATH") = {"h"}$
+- $F_1("http:// HOST_PORT ? SEARCH") = {"h"}$
+- $F_1("http:// HOST_PORT / PATH ? SEARCH") = {"h"}$
+- #text(red)[$"FIRST"_1$ $"FIRST"_1$ konflikt]
+
+*Pre $"FTP_ADDRESS"$*: Len jedno pravidlo — #text(olive)[Žiadny konflikt]
+
+*Pre $"TELNET_ADDRESS"$*: Len jedno pravidlo — #text(olive)[Žiadny konflikt]
+
+*Pre $"MAILTO_ADDRESS"$*: Len jedno pravidlo — #text(olive)[Žiadny konflikt]
+
+*Pre $"LOGIN"$*:
+- $F_1("HOST_PORT") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("USER @ HOST_PORT") = F_1("USER") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- $F_1("USER : PASSWORD @ HOST_PORT") = F_1("USER") = {0, dots, 9, a, dots, z, A, dots, Z}$
+- #text(red)[$"FIRST"_1$ $"FIRST"_1$ konflikt]
+
+= Lexikálna analýza
+
+== Jednoduché
+
+Protokoly:
+- ```bnf http → "http://"```
+- ```bnf ftp → "ftp://"```
+- ```bnf telnet → "telnet://"```
+- ```bnf mailto → "mailto:"```
+
+Rozdelovacie symboly:
+- ```bnf ":"```
+- ```bnf "?"```
+- ```bnf "."```
+- ```bnf "@"```
+- ```bnf "/"```
+- ```bnf "+"```
+
+Relevantné regexy sú triviálne, takže tu nebudú uvedené.
+
+== Identifikátory
+
+Keďže sa ```bnf alpha```, ```bnf digit``` a ```bnf xalpha``` nikdy nevyskytujú osamotene, budú definované tokeny pre ```bnf digits``` (pre definíciu ```bnf port```) a ```bnf xalphas``` pre ostatné výskyty. Definícia pre ```bnf segment``` síce obsahuje ```bnf {xalpha}```, ale toto je ekvivalentné ```bnf [xalphas]```. Budú teda definované tokeny ```bnf TEXT``` a ```bnf NUMBER```
+
+
+Takže gramatika pre identifikátory je následovná:
+
+```bnf
+text → {"A" | .. | "Z" | "a" | .. | "z" | "0" | .. | "9"}
+number → {"0" | .. | "9"}
+```
+
+A regex je teda:
+
+```js
+text = /[A-Za-z0-9]+/
+number = /[0-9]+/
+```
+
+= Syntaktická analýza
+
+Základná gramatika priamo odvodená z BNF s použitím definovaných tokenov. Keďže niektoré pravidlá obsahujú symbol pre nula alebo jeden výskyt aj popri inom obsahu, boli duplikované  pre všetky možné variácie.
+
+```bnf
+URL → HTTP_ADDRESS | FTP_ADDRESS | TELNET_ADDRESS | MAILTO_ADDRESS
+
+HTTP_ADDRESS → http HOST_PORT
+HTTP_ADDRESS → http HOST_PORT "/" PATH
+HTTP_ADDRESS → http HOST_PORT "?" SEARCH
+HTTP_ADDRESS → http HOST_PORT "/" PATH "?" SEARCH
+
+FTP_ADDRESS → ftp LOGIN "/" PATH
+TELNET_ADDRESS → telnet LOGIN
+MAILTO_ADDRESS → mailto text "@" HOSTNAME
+
+LOGIN → HOST_PORT
+LOGIN → USER "@" HOST_PORT
+LOGIN → USER ":" PASSWORD "@" HOST_PORT
+
+HOST_PORT → HOSTNAME | HOSTNAME ":" PORT
+HOSTNAME → text | text "." HOSTNAME
+PORT → DIGITS
+PATH → SEGMENT | SEGMENT "/" PATH
+SEARCH → text | text "+" SEARCH
+USER → text
+PASSWORD → text
+SEGMENT → text | ε
+```
+
+== Konverzia na LL1
+
---
+++ src/grammar.ts
@@ -0,0 +1,266 @@
+import { ensureKey } from "kompa"
+import { readFile } from "node:fs/promises"
+import { inspect } from "node:util"
+
+
+export class Parser {
+    public index = 0
+    public match: RegExpMatchArray = null!
+
+    public matches(pattern: RegExp) {
+        pattern.lastIndex = this.index
+        const match = pattern.exec(this.input)
+
+        if (match) {
+            this.match = match
+            this.index = pattern.lastIndex
+            return true
+        }
+
+        return false
+    }
+
+    public isEOF() {
+        return this.index >= this.input.length
+    }
+
+    constructor(
+        public readonly input: string,
+    ) { }
+}
+
+export const EPSILON = Symbol("EPSILON")
+
+export class SymbolRange {
+    protected static _flyCache = new Map<string, SymbolRange>()
+
+    public *getValues() {
+        const a = this.from.charCodeAt(0)
+        const b = this.to.charCodeAt(0)
+
+        for (let i = a; i <= b; i++) {
+            yield String.fromCharCode(i)
+        }
+    }
+
+    constructor(
+        public readonly from: string,
+        public readonly to: string,
+    ) {
+        return ensureKey(SymbolRange._flyCache, from + "\0" + to, () => this)
+    }
+}
+
+export type RuleSymbol = NonTerminal | string | SymbolRange | typeof EPSILON
+
+export class Rule {
+    public readonly symbols: RuleSymbol[] = []
+
+    protected _first: Set<RuleSymbol> | null = null
+
+    public [inspect.custom]() {
+        return this.symbols.map(v => v == EPSILON ? (
+            "ε"
+        ) : typeof v == "string" ? (
+            JSON.stringify(v)
+        ) : v instanceof SymbolRange ? (
+            JSON.stringify(v.from) + ".." + JSON.stringify(v.to)
+        ) : v.name).join(" . ")
+    }
+}
+
+export class NonTerminal {
+    public readonly rules: Rule[] = []
+
+    public createRule() {
+        const rule = new Rule()
+        this.rules.push(rule)
+        return rule
+    }
+
+    constructor(
+        public readonly name: string,
+    ) { }
+}
+
+export class Grammar {
+    protected readonly _nonTerminals = new Map<string, NonTerminal>()
+
+    public nonTerminals() { return this._nonTerminals.values() }
+
+    public getNonTerminal(name: string) {
+        return ensureKey(this._nonTerminals, name, () => new NonTerminal(name))
+    }
+}
+
+export function parseGrammar(input: string) {
+    const parser = new Parser(input)
+
+    const grammar = new Grammar()
+    let nonTerminal: NonTerminal | null = null
+    let rule: Rule | null = null
+
+    while (!parser.isEOF()) {
+        if (parser.matches(/[\s\n]+/y)) {
+            continue
+        }
+
+        if (parser.matches(/#[^\n]*/y)) {
+            continue
+        }
+
+        if (parser.matches(/\b([a-zA-Z_']+)\b (:=|→)/y)) {
+            nonTerminal = grammar.getNonTerminal(parser.match[1])
+            if (rule) rule = null
+            continue
+        }
+
+        if (parser.matches(/"(.)" *\| *\.+ *\| *"(.)"/y)) {
+            if (rule != null) throw new Error(`Unexpected range construct at ${parser.index}`)
+            if (nonTerminal == null) throw new Error(`Unexpected symbol at ${parser.index}`)
+            nonTerminal.createRule().symbols.push(new SymbolRange(parser.match[1], parser.match[2]))
+            continue
+        }
+
+        if (parser.matches(/"(.*?)"/y)) {
+            if (nonTerminal == null) throw new Error(`Unexpected symbol at ${parser.index}`)
+            rule ??= nonTerminal.createRule()
+            rule.symbols.push(parser.match[1])
+            continue
+        }
+
+        if (parser.matches(/ε/y)) {
+            if (nonTerminal == null) throw new Error(`Unexpected symbol at ${parser.index}`)
+            rule ??= nonTerminal.createRule()
+            rule.symbols.push(EPSILON)
+            continue
+        }
+
+        if (parser.matches(/\b([A-Za-z_']+)\b/y)) {
+            if (nonTerminal == null) throw new Error(`Unexpected symbol at ${parser.index}`)
+            rule ??= nonTerminal.createRule()
+            rule.symbols.push(grammar.getNonTerminal(parser.match[1]))
+            continue
+        }
+
+        if (parser.matches(/\|/y)) {
+            rule = null
+            continue
+        }
+
+        // oxlint-disable-next-line no-debugger
+        debugger
+        throw new Error(`Unexpected "${parser.input[parser.index]}" at ${parser.index}`)
+    }
+
+    return grammar
+}
+
+function print(...msgs: any[]) {
+    // oxlint-disable-next-line no-console
+    console.log(...msgs)
+}
+
+void (async () => {
+    const grammar = parseGrammar(await readFile(process.argv[2], "utf-8"))
+    print(inspect(grammar["_nonTerminals"], false, Infinity, true))
+
+    function symbolToMath(symbol: RuleSymbol) {
+        if (typeof symbol == "string") {
+            return JSON.stringify(symbol)
+        } else if (symbol == EPSILON) {
+            return "ε"
+        } else if (symbol instanceof NonTerminal) {
+            return JSON.stringify(symbol.name)
+        } else {
+            return JSON.stringify(symbol.from) + ", dots, " + JSON.stringify(symbol.to)
+        }
+    }
+
+    const first = new Map<NonTerminal, Set<RuleSymbol>>()
+    const solutions = new Map<NonTerminal, string>()
+    const pending = [...grammar.nonTerminals()]
+    while (true) {
+        let prevSize = pending.length
+
+        pendingLoop: for (const nonTerminal of [...pending]) {
+            print("Processing " + nonTerminal.name)
+            if (nonTerminal.name == "XALPHA") {
+                // oxlint-disable-next-line no-debugger
+                debugger
+            }
+
+            let solution: string[] = []
+            const finalResult = new Set<RuleSymbol>()
+
+            let solutionPart = new Set<string>
+            for (const part of nonTerminal.rules) {
+                print("- Rule:", part)
+                let firstSymbol = part.symbols[0]
+
+                if (firstSymbol == EPSILON || typeof firstSymbol == "string" || firstSymbol instanceof SymbolRange) {
+                    print("-- Trivial")
+                    if (typeof firstSymbol == "string") firstSymbol = firstSymbol[0]
+                    finalResult.add(firstSymbol)
+                    solutionPart.add(`{${symbolToMath(firstSymbol)}}`)
+                    continue
+                }
+
+                const compound: RuleSymbol[] = []
+                for (const symbol of part.symbols) {
+                    print("-- Symbol:", symbolToMath(symbol))
+
+                    if (symbol == nonTerminal) {
+                        print("--- SKIP: Circular reference")
+                        continue
+                    }
+
+                    if (symbol instanceof NonTerminal) {
+                        const symbolValue = first.get(symbol)
+                        if (symbolValue == null) {
+                            print("--- ABORT: Cannot find " + symbol.name)
+                            continue pendingLoop
+                        }
+
+                        compound.push(symbol)
+                        for (const value of symbolValue) finalResult.add(value)
+
+                        if (!symbolValue.has(EPSILON)) {
+                            print("--- No epsilon, finish")
+                            break
+                        }
+
+                        continue
+                    }
+
+                    compound.push(symbol)
+                    finalResult.add(symbol)
+                    break
+                }
+
+                for (const v of compound.map((v, i, a) => v instanceof NonTerminal ? `F_1(${symbolToMath(v)})${i < a.length - 1 ? " / {ε}" : ""}` : `${symbolToMath(v)}`)) {
+                    solutionPart.add(v)
+                }
+            }
+
+            solution.push([...solutionPart].join(" union "))
+            solution.push(`{${[...finalResult].map(symbolToMath).join(", ")}}`)
+            solutions.set(nonTerminal, solution.join(" = "))
+            first.set(nonTerminal, finalResult)
+            pending.splice(pending.indexOf(nonTerminal), 1)
+        }
+
+        if (prevSize == pending.length) {
+            print("Infinite loop, remaining: " + pending.map(v => v.name).join(", "))
+            break
+        }
+
+        if (pending.length == 0) {
+            break
+        }
+    }
+
+    for (const nonTerminal of grammar.nonTerminals()) {
+        print(`F_1(${JSON.stringify(nonTerminal.name)}) = ${solutions.get(nonTerminal) ?? "???"}`)
+    }
+})()
